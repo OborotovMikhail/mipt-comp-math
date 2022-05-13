cba1bae506b5755d4cf96b1d146d241f897a58ac
OborotovMikhail <oborotoffm@yandex.ru>
2022-05-13 22:11:17 +0300

    updated and restructured a little, now seems good

--- Lab_2.1/Lab_2.1.py
+++ Lab_2.1/Lab_2.1.py
@@ -2,91 +2,98 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.optimize as opt
 
+############### Parameters ###############
+
 rate = [0.04, 30000000., 10000.] # Reaction rate (speed) vector (set as a parameter)
 
 stepsNum = 3000 # Number of steps (set as a parameter)
 timeLimit = 0.3 # Time limit in seconds (set as a parameter)
 
-t = timeLimit / stepsNum # Time step
+tStep = timeLimit / stepsNum # Time step
 
 # ODE (Ordinary differential equation) system (set as a parameter)
-def dy1(y):
+def dy1(t, y):
     return -rate[0] * y[0] + rate[2] * y[1] * y[2]
-def dy2(y):
+def dy2(t, y):
     return rate[0] * y[0] - rate[2] * y[1] * y[2] - rate[1] * y[1] ** 2
-def dy3(y):
+def dy3(t, y):
     return rate[1] * y[1] ** 2
 
 # Initial conditions for y (set as a parameter)
 yInit = [1, 0, 0]
 
 # Butcher matrix for Runge-Kutta method (set as a parameter)
-a = [[1/2, 0], 
-     [-1/2, 1]]
-b = [-1/2, 3/2]
-cButch = [1/2, 3/2]
+aButcher = [[1/2, 0], 
+            [-1/2, 1]]
+bButcher = [-1/2, 3/2]
+cButcher = [1/2, 3/2]
+
+############### Functions ###############
 
 # First step
-def rungeKuttaFirstStep(y):
-    return (1 + t * rate[2] * y[1] * y[2] - t * rate[0] * y[0] - y[0],
-            0 + t * rate[0] * y[0] - t * rate[2] * y[1] * y[2] - t * rate[1] * y[1] ** 2 - y[1], 
-            0 + t * rate[1] * y[1] ** 2 - y[2])
+def rungeKuttaFirstStep(y, tStep):
+    return (1 + tStep * rate[2] * y[1] * y[2] - tStep * rate[0] * y[0] - y[0],
+            0 + tStep * rate[0] * y[0] - tStep * rate[2] * y[1] * y[2] - tStep * rate[1] * y[1] ** 2 - y[1], 
+            0 + tStep * rate[1] * y[1] ** 2 - y[2])
 
 # Runge-Kutta method function
-def rungeKutta(y, i, t, c, a, b, cButch):
+def rungeKutta(y, i, tStep, k, aButcher, bButcher, cButcher):
+    # Calculating k for this step
+    k[0][i][0] = dy1(tStep * i + tStep * cButcher[0],
+                     y + tStep * aButcher[0][0] * k[0][i - 1][0])
+    k[0][i][1] = dy1(tStep * i + tStep * cButcher[1],
+                     y + tStep * aButcher[1][0] * k[0][i - 1][0] + tStep * aButcher[1][1] * k[0][i - 1][1])
+    k[1][i][0] = dy2(tStep * i + tStep * cButcher[0],
+                     y + tStep * aButcher[0][0] * k[1][i - 1][0])
+    k[1][i][1] = dy2(tStep * i + tStep * cButcher[1],
+                     y + tStep * aButcher[1][0] * k[1][i - 1][0] + tStep * aButcher[1][1] * k[1][i - 1][1])
+    k[2][i][0] = dy3(tStep * i + tStep * cButcher[0],
+                     y + tStep * aButcher[0][0] * k[2][i - 1][0])
+    k[2][i][1] = dy3(tStep * i + tStep * cButcher[1],
+                     y + tStep * aButcher[1][0] * k[2][i - 1][0] + tStep * aButcher[1][1] * k[2][i - 1][1])
 
-    c[0][i][0] = dy1(y + t * a[0][0] * c[0][i - 1][0])
-    c[0][i][1] = dy1(y + t * a[1][0] * c[0][i - 1][0] + t * a[1][1] * c[0][i - 1][1])
-    c[1][i][0] = dy2(y + t * a[0][0] * c[1][i - 1][0])
-    c[1][i][1] = dy2(y + t * a[1][0] * c[1][i - 1][0] + t * a[1][1] * c[1][i - 1][1])
-    c[2][i][0] = dy3(y + t * a[0][0] * c[2][i - 1][0])
-    c[2][i][1] = dy3(y + t * a[1][0] * c[2][i - 1][0] + t * a[1][1] * c[2][i - 1][1])
-
-    sums = [] # Initializing array of sums of b_i * c_i
-
+    # Initializing array of sums of b_i * k_i, and calculating them
+    sums = []
     for j in range(3):
-        sums.append(b[0] * c[j][i][0] + b[1] * c[j][i][1])
+        sums.append(bButcher[0] * k[j][i][0] + bButcher[1] * k[j][i][1])
 
     # Calculating result
-    return (y[0] + t * (sums[0]),
-            y[1] + t * (sums[1]),
-            y[2] + t * (sums[2]))
+    return (y[0] + tStep * (sums[0]),
+            y[1] + tStep * (sums[1]),
+            y[2] + tStep * (sums[2]))
+
+############### Main calculating cycle ###############
 
 # Initializing c with zeros (for the Runge-Kutta method)
-c = []
+k = []
 for i in range(3):
-    c.append([[0 for i in range(2)] for j in range(stepsNum)])
-
-####### Main calculating cycle #######
+    k.append([[0 for i in range(2)] for j in range(stepsNum)])
 
 # Initializing y with the first step
 yIntegral = []
 yIntegral.append(yInit)
 
 # First step
-yFirstStep =  opt.fsolve(rungeKuttaFirstStep, yIntegral[0])
+yFirstStep =  opt.fsolve(rungeKuttaFirstStep, yIntegral[0], tStep)
 yIntegral.append(yFirstStep)
 
-# Initializing (calculating) for the first steps
+# Initializing (calculating) k for the first steps
 for j in range(3):
-    c[j][0] = [yIntegral[1][j], yIntegral[1][j]]
+    k[j][0] = [yIntegral[1][j], yIntegral[1][j]]
 
-#c[0][0] = [yIntegral[1][0] - yIntegral[0][0], yIntegral[1][0] - yIntegral[0][0]]
-#c[1][0] = [yIntegral[1][1] - yIntegral[0][1], yIntegral[1][1] - yIntegral[0][1]]
-#c[2][0] = [yIntegral[1][2] - yIntegral[0][2], yIntegral[1][2] - yIntegral[0][2]]
-
-# Doing calculations with Runge-Kutta
+# Doing calculations with Runge-Kutta (starting with the 2nd step)
 for i in range(1, stepsNum):
-    yNew = yIntegral[-1]
-    ySolution = rungeKutta(yNew, i, t, c, a, b, cButch)
-    yIntegral.append(ySolution) # Adding to the integral
+    yLastStep = yIntegral[-1]
+    yNewStep = rungeKutta(yLastStep, i, tStep, k, aButcher, bButcher, cButcher)
+    yIntegral.append(yNewStep) # Adding to the integral
 
-# Transposing to get the final result
+############### Plots ###############
+
+# Transposing to plot the final result
 yResult = np.array(yIntegral)
 yResult = np.transpose(yResult)
 
-####### Plots #######
-
+# Plotting
 fig, axs = plt.subplots(2, 2)
 axs[0, 0].plot(yResult[0])
 axs[0, 0].set_title('Axis [0, 0]')
