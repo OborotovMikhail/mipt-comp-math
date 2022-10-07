12558ada2057699828a25a8984742970c57c479c
OborotovMikhail <oborotoffm@yandex.ru>
2022-10-07 15:26:32 +0300

    reworked plotting and comments for task1

--- Lab_1.1/task1.py
+++ Lab_1.1/task1.py
@@ -1,33 +1,41 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-h = 1 / 4 # Шаг сетки (по условию)
+h = 1 / 4 # Grid step (set as a parameter)
 
-# Матрица СЛАУ
-# Посчитана теоретически для дальнейшего сравнения с алгоритмической реализацией
-Matrix = np.array([[4,-1,0,-1,0,0,0,0,0],
-                   [-1,4,-1,0,-1,0,0,0,0],
-                   [0,-1,4,0,0,-1,0,0,0],
-                   [-1,0,0,4,-1,0,-1,0,0],
-                   [0,-1,0,-1,4,-1,0,-1,0],
-                   [0,0,-1,0,-1,4,0,0,-1],
-                   [0,0,0,-1,0,0,4,-1,0],
-                   [0,0,0,0,-1,0,-1,4,-1],
-                   [0,0,0,0,0,-1,0,-1,4]])
-Matrix = Matrix / h / h # учитываем h
-
-print("Матрица СЛАУ:")
-print(Matrix)
-plt.spy(Matrix) # Визуализация матрицы СЛАУ
-plt.show()
-
-# Правая часть системы
+# RHS (right-hand side) of the SLE (set as a parameter)
 Right = np.array([1,1,1,1,1,1,1,1,1])
 
-print("Правая часть системы:")
+# SLE (system of linear equations) matrix
+# Calculated analytically for further comparison with the algorithmic implementation
+Matrix = np.array([[ 4,-1, 0,-1, 0, 0, 0, 0, 0],
+                   [-1, 4,-1, 0,-1, 0, 0, 0, 0],
+                   [ 0,-1, 4, 0, 0,-1, 0, 0, 0],
+                   [-1, 0, 0, 4,-1, 0,-1, 0, 0],
+                   [ 0,-1, 0,-1, 4,-1, 0,-1, 0],
+                   [ 0, 0,-1, 0,-1, 4, 0, 0,-1],
+                   [ 0, 0, 0,-1, 0, 0, 4,-1, 0],
+                   [ 0, 0, 0, 0,-1, 0,-1, 4,-1],
+                   [ 0, 0, 0, 0, 0,-1, 0,-1, 4]])
+Matrix = Matrix / h / h # Taking h (grid step) into account
+
+# Printing SLE matrix
+print("\nSLE matrix:")
+print(Matrix)
+
+# Plotting SLE matrix visualization
+figMatrix, axMatrix = plt.subplots(1, 1)
+figMatrix.canvas.manager.set_window_title('SLE matrix visualization')
+axMatrix.spy(Matrix)
+
+# Printing RHS of the SLE
+print("\nRHS of the SLE:")
 print(Right)
 
-# Решение системы (библиотечной функцией)
+# Solution (using a standart method)
 Solution = np.linalg.solve(Matrix, Right)
-print("Решение:")
+print("\nSLE solution:")
 print(Solution)
+
+# Showing all figures
+plt.show()
