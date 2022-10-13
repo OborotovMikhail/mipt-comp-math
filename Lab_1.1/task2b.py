df5e97a1d4aa3cb0c0f014e1a368c8cae548529e
OborotovMikhail <oborotoffm@yandex.ru>
2022-10-14 00:05:36 +0300

    task2b comments and plots rework

--- Lab_1.1/task2b.py
+++ Lab_1.1/task2b.py
@@ -1,78 +1,83 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-N = 20  # Значение по условию (обратное h)
-h = 1 / N
+N = 20 # Number of steps (set as a parameter)
+h = 1 / N # Grid step
 
-RowLenght = N + 1  # Количество узлов по одной оси
+RowLenght = N + 1 # Number of nodes on 1 axis
 
-Matrix = np.array([])  # Строка значений матрицы
-Valid = np.array([])  # Массив интересующих узлов (не крайние)
+Matrix = np.array([]) # SLE matrix values (as a string, not a matrix)
+Valid = np.array([]) # Array of nodes of interest (all except border nodes)
 
-# Заполняем массив интересующих узлов (НЕ крайних)
-# Крайние узлы не будут рассматриваться с матрице СЛАУ (дают нулевой вклад в нее)
+# Filling an array of valid nodes (the nodes of interest)
+# Border nodes are not of interest as they have a zero contribution to the SLE matrix values
 for i in range(RowLenght):
     for j in range(RowLenght):
-        index = RowLenght * i + j  # Номер узла (строчный)
-        # Включаем только НЕ крайние узлы
+        index = RowLenght * i + j # Node index
+        # Including all nodes except border ones
         if (i != 0 and j != 0 and i != (RowLenght - 1) and j != (RowLenght - 1)):
             Valid = np.append(Valid, index)
 
-print("Интересующие узлы сетки:")
+# Printing idexes of nodes of interest
+print("\nNodes of interest:")
 print(Valid)
 
-# Заполняем матрицу для каждого некрайнего узла
+# Calculating matrix values for each node
 for validIndex in Valid:
     for i in range(RowLenght - 2):
         for j in range(RowLenght - 2):
-            index = RowLenght * (i + 1) + (j + 1)  # Номер узла (строчный)
+            index = RowLenght * (i + 1) + (j + 1) # Node index
 
             if index == validIndex:
-                # Значение в самом узле (центр креста)
+                # Value in the node itself (center of the cross)
                 Matrix = np.append(Matrix, 4)
             elif abs(validIndex - index) == 1 or abs(validIndex - index) == RowLenght:
-                # Соседние элементы и элементы, отличающиеся на длину сетки некрайних узлов
-                # (концы креста)
+                # Adjacent nodes and nodes differing by the length of the grid
+                # (ends of the cross)
                 Matrix = np.append(Matrix, -1)
             else:
-                # Остальные значения = 0
+                # All other values are zero
                 Matrix = np.append(Matrix, 0)
 
-# Преобразование строки в матрицу
-Matrix = Matrix.reshape(len(Valid), len(Valid))
-Matrix = Matrix / h / h  # учитываем h
+Matrix = Matrix.reshape(len(Valid), len(Valid)) # Reshaping to a matrix
+Matrix = Matrix / h / h # Taking h (grid step) into account
 
-print("Получаемая матрица СЛАУ:")
+# Printing SLE matrix
+print("\nCalculated SLE matrix:")
 print(Matrix)
-plt.spy(Matrix)  # Визуализация матрицы СЛАУ
-plt.show()
 
-# Правая часть системы
-Right = np.zeros(Matrix.shape[0])  # Все нули
-Right[int((Right.size - 1) / 2)] = 1  # Кроме центрального узла
+# Plotting SLE matrix visualization
+figMatrix, axMatrix = plt.subplots(1, 1)
+figMatrix.canvas.manager.set_window_title('Calculated SLE matrix visualization')
+axMatrix.set_title('Calculated SLE matrix visualization')
+axMatrix.spy(Matrix)
 
-# Right = np.ones(Matrix.shape[0]) # Случай когда правая часть - все единицы
+# RHS (right-hand side) of the SLE (set as a parameter)
+Right = np.zeros(Matrix.shape[0]) # All zeros
+Right[int((Right.size - 1) / 2)] = 1 # Except the center node
 
-print("Правая часть системы:")
+# Printing RHS of the SLE
+print("\nRHS of the SLE:")
 print(Right)
 
-# Решение системы (библиотечной функцией)
+# Solution (using a standart method)
 Solution = np.linalg.solve(Matrix, Right)
 
-print("Решение:")
+# Printing solution
+print("\nSLE solution:")
 print(Solution)
 
-# Визуализация решения
-fig, ax = plt.subplots()
-# Преобразуем решение
-ReshapedSolutionLen = int(np.sqrt(Solution.size))
-ReshapedSolution = Solution.reshape(ReshapedSolutionLen, ReshapedSolutionLen)
-Pad = np.pad(ReshapedSolution, (1, 1), mode='constant')
-# Цветовая карта
-Colormap = plt.get_cmap('jet')
-# График
-Plot = ax.imshow(Pad, Colormap, aspect='equal', vmin=0, extent=(0, 1, 0, 1), interpolation='gaussian')
-# Цветовая колонка
-fig.colorbar(Plot, ax=ax)
+# Plotting the solution
+ReshapedSolutionLen = int(np.sqrt(Solution.size)) # Reshaped solution length
+ReshapedSolution = Solution.reshape(ReshapedSolutionLen, ReshapedSolutionLen) # Reshaping solution
+Pad = np.pad(ReshapedSolution, (1, 1), mode='constant') # Creating pad (for the plot)
+Colormap = plt.get_cmap('jet') # Colormap
+figSolution, axSolution = plt.subplots(1, 1)
+figSolution.canvas.manager.set_window_title('Solution plot')
+axSolution.set_title('Solution plot')
+Plot = axSolution.imshow(Pad, Colormap, aspect='equal', vmin=0, extent=(0, 1, 0, 1), interpolation='gaussian')
+figSolution.colorbar(Plot, ax=axSolution)
 plt.grid(linestyle='--')
+
+# Showing all figures
 plt.show()
